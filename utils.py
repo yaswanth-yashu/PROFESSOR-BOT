c287b03ec89d25c2fca6e950bb359917dc349182
Yaswanth D <86795414+yaswanth-yashu@users.noreply.github.com>
2023-09-02 22:07:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -138,7 +138,7 @@ def list_to_str(k):
         return ' '.join(f'{elem}, ' for elem in k)
 
 __repo__ = "https://github.com/MrMKN/PROFESSOR-BOT"
-__version__ = "PROFESSOR-BOT ᴠ4.5.0"
+__version__ = "PROFESSOR-BOT"
 __license__ = "GNU GENERAL PUBLIC LICENSE V2"
 __copyright__ = "Copyright (C) 2023-present MrMKN <https://github.com/MrMKN>"
 
